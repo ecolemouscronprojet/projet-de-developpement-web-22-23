cf96c52ed48d73d51927d26c8efe5e34e04f907a
Samuel Lassoie <samuel.lassoie@gmail.com>
2022-10-11 18:34:37 +0200

    wip

--- exercices/exercice1.js
+++ exercices/exercice1.js
@@ -5,9 +5,22 @@
 // 100
 // 1
 
-function calcul(list) {
+function sortFunc() {
+    const tableau = [];
+    for(let i of arguments) {
+        tableau.push(i * 2);
+    }
 
+    tableau.sort((a, b) => a - b);
+
+    return tableau;
 }
+  const result = sortFunc(2,7,1,8,13,18);
+  console.log(result);
+
+// function calcul(list) {
+
+// }
 
 
 // calcul(10, 5, 40, 25, 100, 1, 30)
